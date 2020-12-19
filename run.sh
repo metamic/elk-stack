44574e85f4e847f1e6fa816bc42a495ba37b1001
Meta Hacker <meta.microcode@gmail.com>
2020-12-19 17:37:08 +0900

    change run.sh to use detach when starting docker-compose up

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ source $PWD/.env
 sed -i "s/\$ELASTICSEARCH_PASSWORD/$ELASTICSEARCH_PASSWORD/g" $PWD/kibana/config/kibana.yml
 sed -i "s/\$ELASTICSEARCH_PASSWORD/$ELASTICSEARCH_PASSWORD/g" $PWD/logstash/config/logstash.yml
 
-docker-compose up --build
+docker-compose up --build --detach
