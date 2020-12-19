d5306e6d7071d15ba4b8e00f5ac42fb3059a5cee
root <root@outpost.com>
2020-12-19 23:31:18 +0900

    change conf

--- run.sh
+++ run.sh
@@ -16,5 +16,6 @@ fi
 
 sed -i "s/\$ELASTICSEARCH_PASSWORD/$ELASTICSEARCH_PASSWORD/g" $PWD/kibana/config/kibana.yml
 sed -i "s/\$ELASTICSEARCH_PASSWORD/$ELASTICSEARCH_PASSWORD/g" $PWD/logstash/config/logstash.yml
+sed -i "s/\$ELASTICSEARCH_PASSWORD/$ELASTICSEARCH_PASSWORD/g" $PWD/logstash/pipeline/logstash.conf
 
 docker-compose up --build --detach
