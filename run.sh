d45af5dda326c16c3891b272554f258eb0218c86
Meta Hacker <meta.microcode@gmail.com>
2020-12-19 17:22:41 +0900

    add run.sh script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source $PWD/.env
+
+sed -i "s/\$ELASTICSEARCH_PASSWORD/$ELASTICSEARCH_PASSWORD/g" $PWD/kibana/config/kibana.yml
+sed -i "s/\$ELASTICSEARCH_PASSWORD/$ELASTICSEARCH_PASSWORD/g" $PWD/logstash/config/logstash.yml
+
+docker-compose up --build
